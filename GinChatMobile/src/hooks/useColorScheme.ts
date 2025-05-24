114aa7522dc79ef104b2ebb5f497a7f92381f3d1
yongdilun <dilunyong@gmail.com>
2025-05-24 21:26:53 +0800

    remove unwanted folder

--- GinChatMobile/src/hooks/useColorScheme.ts
+++
@@ -1,8 +0,0 @@
-import { useColorScheme as useNativeColorScheme } from 'react-native';
-
-export type ColorScheme = 'light' | 'dark';
- 
-export function useColorScheme(): ColorScheme {
-  const colorScheme = useNativeColorScheme();
-  return colorScheme ?? 'light';
-}
